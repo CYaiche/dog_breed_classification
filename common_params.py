71f58c6ab46cd1cd5a35defd7c5fec3692666236
Clara <clarayaiche@hotmail.com>
2023-08-15 21:20:20 +0200

    add function to plot and dog within img

--- common_params.py
+++ common_params.py
@@ -1,4 +1,6 @@
-
+import os 
 data_dir = "C:/dev/image_classification/data/"
 
-img_dir = data_dir + "Images/"
+img_dir = os.path.join(data_dir , "Images\\")
+
+annotation_dir = os.path.join(data_dir, "Annotation\\")
