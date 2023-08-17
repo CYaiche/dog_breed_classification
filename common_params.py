64f64b19f88f5256aee3176ac7180badcc9228c2
Clara <clarayaiche@hotmail.com>
2023-08-17 17:31:40 +0200

    add parameters to choose number of classes, first logs with mlflow in transfer learning notebook

--- common_params.py
+++ common_params.py
@@ -1,6 +1,14 @@
 import os 
+
 data_dir = "C:/dev/image_classification/data/"
 
 img_dir = os.path.join(data_dir , "Images\\")
 
 annotation_dir = os.path.join(data_dir, "Annotation\\")
+
+npy_dir  = os.path.join(data_dir, "npy\\")
+
+MAX_NUMBER_CLASS = 30 
+
+DEFAULT_VALIDATION_SPLIT = 0.2
+DEFAULT_TEST_SPLIT = 0.1
--- common_plots.py
+++ common_plots.py
@@ -33,14 +33,52 @@ def display_color_histogram(imgCV):
     plt.hist(red_color, color="red")
     plt.show()
     
-    
-def display_images(img_list,N,M):
+
+def display_images(img_list, N, M, from_path=True, titles=None, main_title=None):
     fig, axs = plt.subplots(N,M)
-    cnt = 0 
-    for i in range(N) : 
-        for j in range(M) : 
-            img = cv2.imread(img_list[cnt])
-            axs[i,j].imshow(cv2.cvtColor(img, cv2.IMREAD_ANYCOLOR))
-            axs[i,j].axis("off")
-            cnt = cnt + 1 
+    cnt = 0
+    for i in range(N): 
+        for j in range(M):  
+            img = cv2.imread(img_list[cnt]) if from_path else img_list[cnt]
+            if N !=1 : 
+                axs[i, j].imshow(cv2.cvtColor(img, cv2.IMREAD_ANYCOLOR))
+                axs[i, j].axis("off")
+                if titles != None : 
+                    axs[i, j].set_title(titles[i+j])
+            else : 
+                axs[j].imshow(cv2.cvtColor(img, cv2.IMREAD_ANYCOLOR))
+                axs[j].axis("off")
+                if titles != None : 
+                    axs[j].set_title(titles[j])
+            cnt = cnt + 1
+    if main_title != None : 
+        y0 = 0.9 if titles != None else 0.7
+        fig.suptitle(main_title,y=y0)
+    plt.show()
+    
+def plot_img(img): 
+
+    plt.axis("off")
+    plt.imshow(cv2.cvtColor(img,cv2.IMREAD_ANYCOLOR))
+    plt.show()
+    
+def plot_img_from_path(img_path): 
+    assert os.path.exists(img_path) , f"image path does not exist {img_path}"
+    imgCV = cv2.imread(img_path)
+
+    plt.axis("off")
+    plt.imshow(cv2.cvtColor(imgCV,cv2.IMREAD_ANYCOLOR))
+    plt.show()
+    
+def plot_img_with_rectangles_from_path(img_path, reg_coor):
+    assert os.path.exists(img_path) , f"image path does not exist {img_path}"
+    imgCV = cv2.imread(img_path)
+    
+    for cor in reg_coor : 
+        pt1  = (int(cor['xmin']), int(cor['ymin']))
+        pt2 =  (int(cor['xmax']), int(cor['ymax']))
+
+        imgCV = cv2.rectangle(imgCV, pt1, pt2, (255, 0, 0), 2)
+    plt.axis("off")
+    plt.imshow(cv2.cvtColor(imgCV,cv2.IMREAD_ANYCOLOR))
     plt.show()
